7b00be3d6254213c1e4c3b7b8d611c1cd91243d9
AndrewMag13 <nnaaiillburdasov@gmail.com>
2024-03-31 22:35:25 +0400

    int

---
+++ src/app/home/page.tsx
@@ -0,0 +1,9 @@
+import { NextPage } from 'next'
+import s from './home.module.scss'
+import { revalidatePath } from 'next/cache'
+
+const HomePage = () => {
+  return <>homepage</>
+}
+
+export default HomePage
