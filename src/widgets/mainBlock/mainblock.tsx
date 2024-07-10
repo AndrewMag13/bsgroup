feca8b982abfda6faf2cc427801320a6c214684a
AndrewMag13 <nnaaiillburdasov@gmail.com>
2024-07-10 09:52:47 +0400

    added animation to mainBlock text

--- src/widgets/mainBlock/mainblock.tsx
+++ src/widgets/mainBlock/mainblock.tsx
@@ -1,16 +1,34 @@
+'use client'
 import Parallax from '@/shared/ui/parallax/parallax'
 import s from './mainblock.module.scss'
 import Napisat from '@/shared/ui/napisat/napisat'
 import TypeWrote from '@/shared/ui/typeWrote/typeWrote'
 import { useTranslations } from 'next-intl'
-
+import { motion } from 'framer-motion'
 const MainBlockPage = () => {
   const t = useTranslations('home.mainBlock')
   return (
     <>
       <div className={s.main} id="main">
         <Parallax />
-        <div className={s.text}>
+        <motion.div
+          className={s.text}
+          initial={{
+            opacity: 0,
+            x: -50,
+          }}
+          whileInView={{
+            opacity: 1,
+            x: 0,
+            transition: {
+              duration: 1,
+            },
+          }}
+          style={{
+            zIndex: 10,
+          }}
+          viewport={{ once: true }}
+        >
           <h1 className={s.title}>
             {t('headerFirst')}
             <b>
@@ -20,7 +38,7 @@ const MainBlockPage = () => {
           </h1>
           <div className={s.smalltext}>{t('smallText')}</div>
           <Napisat />
-        </div>
+        </motion.div>
       </div>
     </>
   )
