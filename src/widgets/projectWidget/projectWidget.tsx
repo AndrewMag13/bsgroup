288459d85345e6d4af1d2768edaa0b6c5451ef0c
AndrewMag13 <nnaaiillburdasov@gmail.com>
2024-07-29 09:51:47 +0400

    reworked photo viewer animations

--- src/widgets/projectWidget/projectWidget.tsx
+++ src/widgets/projectWidget/projectWidget.tsx
@@ -23,7 +23,14 @@ const ProjectWidget = () => {
           className={s.figure}
         />
         <div className={s.cards__container}>
-          <PhotoProvider speed={() => 1000}>
+          <PhotoProvider
+            speed={() => 600}
+            easing={(type) =>
+              type === 2
+                ? 'cubic-bezier(0.36, 0, 0.66, -0.56)'
+                : 'cubic-bezier(0.34, 1.56, 0.64, 1)'
+            }
+          >
             <div className={s.cards}>
               {keys.map((key) => {
                 return (
