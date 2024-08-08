304ccd8ebdda44bf181e9964cd1b1e7cfc1ed187
AndrewMag13 <nnaaiillburdasov@gmail.com>
2024-08-08 20:34:06 +0400

    improved sent apps formatting

--- src/app/[locale]/api/bot/route.ts
+++ src/app/[locale]/api/bot/route.ts
@@ -1,4 +1,5 @@
 import { BOT_API, CHAT_ID } from '@/shared/constants/constants'
+import { FmtString, bold } from 'telegraf/format'
 
 import { Telegraf } from 'telegraf'
 
@@ -6,13 +7,20 @@ const bot = new Telegraf(BOT_API)
 export const dynamic = 'force-dynamic'
 export const POST = async (request: Request) => {
   const body = await request.json()
+//   const str = await new FmtString(
+//     `Имя:    ${bold(body.name)}
+
+// Телефон: ${body.phone}
+
+// Почта:   ${body.email}`,
+//   )
   await bot.telegram.sendMessage(
     CHAT_ID,
-`Имя: **${body.name}**
+`Имя: \`${body.name}\`
 
-Телефон: **${body.phone}**
+Телефон: *${body.phone}*
 
-Почта: **${body.email}**`,
+Почта: *${body.email}*`, {parse_mode: 'Markdown'},
   )
   return Response.json(body)
 }
